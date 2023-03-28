d172bef14c9af2e907fe2e903cc1b4ecbf2520b5
Khomero98 <pulido.juandavid@gmail.com>
2023-03-28 00:35:59 -0500

    juego inicial con escenario umng

---
+++ Juego/Assets/scripts/movimiento_grilla.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento_grilla : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 targetPosition;
+    private float yImput, xImput;
+    private float moveTime=0.15f;
+    private bool isMoving;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        xImput = Input.GetAxisRaw("Horizontal");
+        yImput = Input.GetAxisRaw("Vertical");
+        if((xImput!=0 || yImput!=0)&& !isMoving && Input.anyKeyDown)
+        {
+            calculatetargetposition();
+            if (canmovetoTargetposition())
+            {
+                StartCoroutine(move());
+            }
+            
+        }
+    }
+    IEnumerator move()
+    {
+        isMoving = true;
+        float timeElapsed= 0f;
+        Vector3 startPosition=transform.position;
+        while (timeElapsed < moveTime)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, timeElapsed / moveTime);
+            timeElapsed += Time.deltaTime;
+            yield return null;        
+        }
+        transform.position = targetPosition;
+        isMoving = false;
+    }
+    private void calculatetargetposition()
+    {
+        if (xImput == 1)
+        {
+            targetPosition = transform.position + Vector3.right;
+        }
+        if (xImput == -1)
+        {
+            targetPosition = transform.position + Vector3.left;
+        }
+        if (yImput == 1)
+        {
+            targetPosition = transform.position + new Vector3(0,0,yImput);
+        }
+        if (yImput == -1)
+        {
+            targetPosition = transform.position + new Vector3(0, 0, yImput);
+        }
+    }
+    private bool canmovetoTargetposition()
+    {
+        Collider[] colliders = Physics.OverlapSphere(targetPosition, 0.10f);
+        return colliders.Length == 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(targetPosition, 0.16f);
+    }
+}
